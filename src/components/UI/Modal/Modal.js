bc942a1b02ba2436750687d8531091ac7e0465ac
alexthedar <alex.castaneda@gmail.com>
2017-11-27 16:17:17 -0800

    add Backdrop to Modal.  add purchaseCancelHandler to BurgerBuilder.  add click event to Backdrop.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
+  <Aux>
+  <Backdrop 
+    show={props.show}
+    clicked={props.modalClosed} 
+    />
     <div 
       className={classes.Modal}
       style={{
@@ -10,6 +17,7 @@ const modal = (props) => (
       }}>
       {props.children}
     </div>
+  </Aux>
   );
 
 
