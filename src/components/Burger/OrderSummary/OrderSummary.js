6ed58f8fdad2bcb5ae382fb719ce82d58e5e5e0d
alexthedar <alex.castaneda@gmail.com>
2017-11-27 16:00:09 -0800

    ordersummary dynamically creates list of ingredients for checkoutmodal.  import ordersummary and use in modal tag on burgerbuilder

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Aux from '../../../hoc/Aux';
 
-const orderSummary = () => {
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+        </li> );
+    });
   return (
-    <div>
-      
-    </div>
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Continue to Checkout?</p>
+    </Aux>
   );
 };
 
