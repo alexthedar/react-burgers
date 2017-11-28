97a90c35d42ce85d59ae9162e84ca9153ae87b95
alexthedar <alex.castaneda@gmail.com>
2017-11-28 12:38:37 -0800

    created Logo.css

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+const logo = (props) => {
+  return (
+    <div>
+      <img src={burgerLogo} alt="MyBurger" />
+    </div>
+  );
+};
+
+export default logo;
