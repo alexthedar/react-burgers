adc1e29a43aee42980b7624b8089ae9b2a41a515
alexthedar <alex.castaneda@gmail.com>
2017-12-11 13:47:28 -0800

    add checkValidity for form in ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'You Name',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false        
       },
       zipCode: {
         elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Zip Code',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false        
       },
       country: {
         elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false        
       },
       email: {
         elementType: 'input',
@@ -46,7 +64,11 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'You Email',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false        
       },
       deliveryMethod: {
         elementType: 'select',
@@ -91,6 +113,20 @@ class ContactData extends Component {
       });
   }
 
+  checkValidity (value, rules) {
+    let isValid = false;
+    if(rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if(rules.minLength) {
+      isValid = value >= rules.minLength && isValid;
+    }
+    if(rules.maxLength) {
+      isValid = value <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
   inputChangedHandler = (e, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -99,6 +135,7 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = e.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({orderForm: updatedOrderForm});
   }
