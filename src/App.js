6da6412a5c60d1d95acfde6efdc18f733cd18801
alexthedar <alex.castaneda@gmail.com>
2018-06-05 14:56:05 -0700

    redux done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import {Route, Switch} from 'react-router-dom';
 import Orders from './containers/Orders/Orders';  
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -17,6 +18,9 @@ class App extends Component {
             <Route 
               path='/orders'
               component={Orders} />
+            <Route 
+              path='/auth'
+              component={Auth} />
             <Route 
               path="/" 
               exact 
