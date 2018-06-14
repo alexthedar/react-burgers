fa995ebec07a2447931c3fb7a908d4fffde57077
alexthedar <alex.castaneda@gmail.com>
2018-06-14 15:25:00 -0700

    auth token, userid token, spinner, error message

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import thunk from 'redux-thunk';
 
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
 
 export const authStart = () => {
   return  {
@@ -6,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   };
 };
 
@@ -20,8 +23,24 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAcoay3PKcG_SRKXhrR45qzW1rzZjWMDEw';
+    if(!isSignup) { url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAcoay3PKcG_SRKXhrR45qzW1rzZjWMDEw'};
+    axios.post(url, authData)
+        .then(response => {
+          console.log(response);
+          dispatch(authSuccess(response.data.idToken, response.data.localId))
+        })
+        .catch(err => {
+          console.log(err)
+          dispatch(authFail(err.response.data.error))
+        })
   };
 };
