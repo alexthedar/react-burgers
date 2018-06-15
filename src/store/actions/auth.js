1cd16e8d336a5364b1816853a887cea9edc6ea2e
alexthedar <alex.castaneda@gmail.com>
2018-06-15 14:48:45 -0700

    localstorage for token, userid and expiretime

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -79,13 +79,13 @@ export const authCheckState = () => {
     const token = localStorage.getItem('token')
     if(!token) { dispatch(logout())} 
     else { 
-      const expirationDate = localStorage.getItem('expirationDate')
-      if(expirationDate > new Date()){
+      const expirationDate = new Date(localStorage.getItem('expirationDate'))
+      if(expirationDate <= new Date()){
+        dispatch(logout())
+      } else {
         const userId = localStorage.getItem('userId')
         dispatch(authSuccess(token, userId))
-        dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
-      } else {
-        dispatch(logout())
+        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000))
       }
     }
   }
